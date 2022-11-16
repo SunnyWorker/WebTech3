4af9a3038897aabb6371c2e7797330897ab2cf8a
SunnyWorker <artem.khotin@gmail.com>
2022-11-16 16:37:26 +0300

    Initial commit

---
+++ Server/src/main/java/org/example/ClientsConnection.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+
+public class ClientsConnection {
+    private List<Client> clients;
+
+    private ServerSocket serverSocket;
+}
